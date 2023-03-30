15953a2966ed1193109f41e27cbf7c38cce0c227
lai2-01 <lai2-01@itismeucci.com>
2023-03-30 14:23:46 +0200

    problema risolto

--- stampa.cpp
+++ stampa.cpp
@@ -8,12 +8,13 @@ Lorenzo Rettori  30/03/2023
 using namespace std;
 int main()
 {
-  int i=10 ,n;
+  int i=0 ,n;
   cout<<"Quante volte voi stampare la frase? ";
   cin>>n;
   while (i<n)
   {
-   cout<<"Bada come la fuma"<<endl; 
+   cout<<"Bada come la fuma"<<endl;
+	i++; 
   }
   system("pause");
 }
